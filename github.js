dbf1a960d2005e54d5a2332a965a282bfd8c2e16
rupam sahriar <rupamsahriar@gmail.com>
2020-06-08 02:43:53 +0600

    change 4th time

---
+++ github.js
@@ -0,0 +1,18 @@
+// …or create a new repository on the command line
+
+// echo "# testing_only" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git remote add origin https://github.com/rupam71/testing_only.git
+// git push -u origin master
+                
+
+// …or push an existing repository from the command line
+
+// git remote add origin https://github.com/rupam71/testing_only.git
+// git push -u origin master
+
+// …or import code from another repository
+
+// You can initialize this repository with code from a Subversion, Mercurial, or TFS project.
